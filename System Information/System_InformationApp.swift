bd308315f6784bf64c27a8edf4b92fa7117ea6af
Chris <zhris@zhr.is>
2024-11-18 15:21:15 -0600

    Initial Commit

---
+++ System Information/System_InformationApp.swift
@@ -0,0 +1,17 @@
+//
+//  System_InformationApp.swift
+//  System Information
+//
+//  Created by Chris on 11/18/24.
+//
+
+import SwiftUI
+
+@main
+struct System_InformationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
